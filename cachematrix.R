b4cab0c1842bcdcd188c42aff547ca7603082e6a
dmaharana <dmaharana@gmail.com>
2015-04-22 01:02:43 +0530

    2nd commit with cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -26,5 +26,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## change in the matrix then it returns the cached inverse
 
 cacheSolve <- function(x, ...) {
-
+        inverted_matrix <- x$get_inverse()
+        
+        if(!is.null(inverted_matrix)){
+          message("Got inverted matrix")
+          return(inverted_matrix)
+        }
+        matrix2inverse <- x$get()
+        inverted_matrix <- solve(matrix2inverse)
+        x$set_inverse(inverted_matrix)
+        inverted_matrix
 }
